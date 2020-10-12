03d238193d664eb55641c42f53cb4727c3732892
Febilenia <febileniarismapa@gmail.com>
2020-10-12 07:31:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/todolistapplication/DialogCloseListener.java
@@ -0,0 +1,8 @@
+package com.example.todolistapplication;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
+
